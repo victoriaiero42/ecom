fb8e5b3b34bb4ae75c5da19994b739079bc654ef
Victoria <victoriaiero@gmail.com>
2021-01-31 17:16:09 +0300

    change color cart item

--- server/models/product.js
+++ server/models/product.js
@@ -49,7 +49,7 @@ const productSchema = new mongoose.Schema({
   },
   color: {
     type: String,
-    enum: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+    enum: ['Black', 'Brown', 'Silver', 'White', 'Blue', 'Pink', 'Golden', 'Space Gray'],
   },
   brand: {
     type: String,
