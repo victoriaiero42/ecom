0a9a7e4c3815dcbd1252785d394dea930e9aca36
Victoria <victoriaiero@gmail.com>
2021-02-09 01:17:55 +0300

    handle remove

--- client/src/pages/admin/coupon/CreateCouponPage.js
+++ client/src/pages/admin/coupon/CreateCouponPage.js
@@ -12,21 +12,48 @@ export default function CreateCouponPage() {
   const [expiry, setExpiry] = useState("");
   const [discount, setDiscount] = useState("");
   const [loading, setLoading] = useState(false);
+  const [coupons, setCoupons] = useState([]);
 
-  const { user } = useSelector(state => ({ ...state }));
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    getCoupons().then((res) => {
+      setCoupons(res.data);
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
     console.log(name, expiry, discount);
     createCoupon({ name, expiry, discount }, user.token)
-      .then(res => {
+      .then((res) => {
         setLoading(false);
-        setName('');
-        setDiscount('');
-        setExpiry('');
-        toast.success(`"${res.data.name}" is created.`)
-      }).catch(err => console.log('create coupon error', err))
+        getCoupons().then((res) => {
+          setCoupons(res.data);
+        });
+        setName("");
+        setDiscount("");
+        setExpiry("");
+        toast.success(`"${res.data.name}" is created.`);
+      })
+      .catch((err) => console.log("create coupon error", err));
+  };
+
+  const handleRemove = (couponId) => {
+    if (window.confirm("Delete?")) {
+      setLoading(true);
+
+      removeCoupon(couponId, user.token).then((res) => {
+        getCoupons()
+          .then((res) => {
+            setCoupons(res.data);
+            setLoading(false);
+            toast.error(` Coupon "${res.data.name}" has been deleted.`);
+          })
+          .catch((err) => console.log(err));
+      });
+    }
   };
 
   return (
@@ -36,7 +63,11 @@ export default function CreateCouponPage() {
           <AdminNav />
         </div>
         <div className="col-md-10">
-          <h4>Coupon</h4>
+          { loading ? (
+            <h4 className="text-danger">loading...</h4>
+          ) : (
+              <h4>Coupon</h4>
+            ) }
 
           <form onSubmit={ handleSubmit }>
             <div className="form-group">
@@ -74,6 +105,40 @@ export default function CreateCouponPage() {
             </div>
             <button className="btn btn-outline-primary">Save</button>
           </form>
+
+          <br />
+          <h4>{ coupons.length } Coupons</h4>
+
+          <table className="table table-bordered">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Expiry</th>
+                <th scope="col">Discount</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              { coupons
+                ? coupons.map((c) => {
+                  return (
+                    <tr key={ c._id }>
+                      <td>{ c.name }</td>
+                      <td>{ new Date(c.expiry).toLocaleDateString() }</td>
+                      <td>{ c.discount }</td>
+                      <td>
+                        <DeleteOutlined
+                          onClick={ () => handleRemove(c._id) }
+                          className="text-danger pointer"
+                        />
+                      </td>
+                    </tr>
+                  );
+                })
+                : "" }
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
